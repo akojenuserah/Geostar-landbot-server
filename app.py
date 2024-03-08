63707e1b9c2665699e28d73a8ae9a2c2da694a87
salimchoura <salim.choura@yahoo.com>
2024-03-08 00:04:44 -0700

    added capitalization and removed punctuation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import pandas as pd
-
+import string
 data_csv = pd.read_csv('new_data.csv',low_memory=False)
 frame = pd.DataFrame(data_csv)
 
@@ -11,6 +11,10 @@ app = Flask(__name__)
 @app.route('/lgas')    
 def lgas():
     unique_lga = frame['LGA'].unique().tolist()
+    unique_lga.append('go back')
+    for i in range(len(unique_lga)):
+        unique_lga[i] = unique_lga[i].translate(str.maketrans('', '', string.punctuation))
+        unique_lga[i] = unique_lga[i].capitalize()
     return unique_lga
 
 #done
@@ -18,6 +22,9 @@ def lgas():
 def ward(lga):
     associated_wards = frame[data_csv['LGA'] == lga]['Ward'].unique().tolist()
     associated_wards.append('go back')
+    for i in range(len(associated_wards)):
+        associated_wards[i] = associated_wards[i].translate(str.maketrans('', '', string.punctuation))
+        associated_wards[i] = associated_wards[i].capitalize()
     return associated_wards
 #done
 @app.route('/lga/ward/<ward>')    
@@ -28,6 +35,9 @@ def hospitals(ward):
     while 'NaN' in associated_hospitals:
         associated_hospitals.remove('NaN')
     associated_hospitals.append('go back')
+    for i in range(len(associated_hospitals)):
+        associated_hospitals[i] = associated_hospitals[i].translate(str.maketrans('', '', string.punctuation))
+        associated_hospitals[i] = associated_hospitals[i].capitalize()
     return associated_hospitals
 
 #done
@@ -68,6 +78,9 @@ def hospital_resources(hospital):
 def settlements(hospital):
     settlements = frame[frame['Health Facility'] == hospital]['Settlement'].unique().tolist()
     settlements.append('go back')
+    for i in range(len(settlements)):
+        settlements[i] = settlements[i].translate(str.maketrans('', '', string.punctuation))
+        settlements[i] = settlements[i].capitalize()
     return settlements
 
 #done
